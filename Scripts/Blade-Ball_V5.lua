8178dd977816731ffde65d4d3895acdf3f29a120
xiaoxuxu2333 <146357705+xiaoxuxu2333@users.noreply.github.com>
2023-10-27 03:01:26 +0800

    Update Blade-Ball_V5.lua

--- Scripts/Blade-Ball_V5.lua
+++ Scripts/Blade-Ball_V5.lua
@@ -278,9 +278,9 @@ table.insert(coroutines, task.spawn(function()
 		local platform = Instance.new("Part")
 		platform.Anchored = true
 		if map:FindFirstChild("BottomCircle") then
-			platform.Position = map.BottomCircle.Position-(Vector3.yAxis*14)
+			platform.Position = map.BottomCircle.Position-(Vector3.yAxis*13.5)
 		else
-			platform.Position = map.BALLSPAWN.Position-(Vector3.yAxis*14)
+			platform.Position = map.BALLSPAWN.Position-(Vector3.yAxis*13.5)
 		end
 		platform.Size = Vector3.new(2048, 1, 2048)
 		platform.Parent = map
