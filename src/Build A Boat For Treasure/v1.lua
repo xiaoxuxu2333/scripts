36e7a78214f572cae15e80705ed9f821ffa22c23
xiaoxuxu2333 <146357705+xiaoxuxu2333@users.noreply.github.com>
2024-09-08 17:42:51 +0800

    Update v1.lua

--- src/Build A Boat For Treasure/v1.lua
+++ src/Build A Boat For Treasure/v1.lua
@@ -3,6 +3,9 @@ local RunService = game:GetService("RunService")
 
 local localPlayer = Players.LocalPlayer
 
+local goldBlock = localPlayer:WaitForChild("Data"):WaitForChild("GoldBlock")
+local gold = localPlayer.Data:WaitForChild("Gold")
+
 local claimRiverResultsGoldEvent = workspace:WaitForChild("ClaimRiverResultsGold")
 local stagePositions = {}
 local chestTrigger, chestTriggerOriginCFrame
@@ -49,11 +52,13 @@ main:CreateToggle("自动刷金条&块", function(enabled)
 	platform.Size = Vector3.new(4, 1, 4)
 	platform.Parent = workspace
 
-	local oldGold = localPlayer.Data.Gold.Value
+	local oldGold = gold.Value
 	local timer = 0
 	local root = localPlayer.Character.HumanoidRootPart
 	local unlockChest, characterAdded
-	local update = RunService.Heartbeat:Connect(function()
+	local connections = {}
+
+	table.insert(connections, RunService.Heartbeat:Connect(function()
 		if unlockChest and root.Parent then
 			chestTrigger.CFrame = root.CFrame
 			--firetouchinterest(chestTrigger, root, 0)
@@ -64,9 +69,9 @@ main:CreateToggle("自动刷金条&块", function(enabled)
 		local t = time()
 		if t - timer > 60 then
 			timer = t
-			status["每分钟金条"] = localPlayer.Data.Gold.Value - oldGold
-			status["每小时金条"] = (localPlayer.Data.Gold.Value - oldGold) * 60
-			oldGold = localPlayer.Data.Gold.Value
+			status["每分钟金条"] = gold.Value - oldGold
+			status["每小时金条"] = (gold.Value - oldGold) * 60
+			oldGold = gold.Value
 		end
 		
 		for i = 1, #stagesData do
@@ -78,9 +83,7 @@ main:CreateToggle("自动刷金条&块", function(enabled)
 			info = info .. string.format("%s: %s\n", stat, value)
 		end
 		text.Text = info
-	end)
-
-	local connections = {}
+	end))
 
 	table.insert(connections, localPlayer.CharacterAdded:Connect(function(newChar)
 		root = newChar:WaitForChild("HumanoidRootPart")
@@ -125,10 +128,86 @@ main:CreateToggle("自动刷金条&块", function(enabled)
 		status["用时时间"] = string.format("%.4f秒", time() - startTime)
 	end
 
-	update:Disconnect()
 	for _, connection in connections do
 		connection:Disconnect()
 	end
 	text:Destroy()
 	platform:Destroy()
+	chestTrigger.CFrame = chestTriggerOriginCFrame
+end)
+
+main:CreateToggle("自动刷金块", function(enabled)
+	goldBlockFarming = enabled
+	if not goldBlockFarming then return end
+
+	local status = {}
+
+	local text = Drawing.new("Text")
+	text.Outline = true
+	text.OutlineColor = Color3.new(0, 0, 0)
+	text.Color = Color3.new(1, 1, 1)
+	text.Center = false
+	text.Position = Vector2.new(50, 50)
+	text.Text = ""
+	text.Visible = true
+
+	local platform = Instance.new("Part")
+	platform.Anchored = true
+	platform.Size = Vector3.new(4, 1, 4)
+	platform.Parent = workspace
+
+	local oldGold = goldBlock.Value
+	local timer = 0
+	local root = localPlayer.Character.HumanoidRootPart
+	local characterAdded
+
+	local connections = {}
+
+	table.insert(connections, localPlayer.CharacterAdded:Connect(function(newChar)
+		root = newChar:WaitForChild("HumanoidRootPart")
+		platform.CFrame = stagePositions[1]
+		root.CFrame = platform.CFrame * CFrame.new(0, 10, 0)
+	end))
+
+	table.insert(connections, localPlayer.PlayerGui.ChildAdded:Connect(function(newGui)
+		if newGui.Name == "RiverResultsGui" then
+			newGui:WaitForChild("LocalScript").Enabled = false
+		end
+	end))
+
+	platform.CFrame = stagePositions[1]
+	root.CFrame = platform.CFrame * CFrame.new(0, 10, 0)
+	task.wait(7.6)
+
+	table.insert(connections, RunService.Heartbeat:Connect(function()
+		if root.Parent then
+			chestTrigger.CFrame = root.CFrame
+			--firetouchinterest(chestTrigger, root, 0)
+		end
+		
+		local t = time()
+		if t - timer > 60 then
+			timer = t
+			status["每分钟金块"] = goldBlock.Value - oldGold
+			status["每小时金块"] = (goldBlock.Value - oldGold) * 60
+			oldGold = goldBlock.Value
+		end
+		
+		local info = ""
+		for stat, value in status do
+			info = info .. string.format("%s: %s\n", stat, value)
+		end
+		text.Text = info
+	end))
+	
+	while goldBlockFarming do
+		task.wait()
+	end
+
+	for _, connection in connections do
+		connection:Disconnect()
+	end
+	text:Destroy()
+	platform:Destroy()
+	chestTrigger.CFrame = chestTriggerOriginCFrame
 end)
