c62edf9fe1d5ed904bb697caa7fdf835b43dfd5a
xiaoxuxu2333 <146357705+xiaoxuxu2333@users.noreply.github.com>
2024-10-02 13:12:07 +0800

    更新 src

---
+++ src/Salted-Fish-Dreamer-Simulator/cook.lua
@@ -0,0 +1,41 @@
+local LocalPlayer = game:GetService("Players").LocalPlayer
+
+local Kitchen = game:GetService("Workspace").Stuffs.Kitchen
+local ThirteenSpicesCD = Kitchen.ThirteenSpices.ClickDetector
+
+local UILib = getgenv().UILibCache or loadstring(game:HttpGet("https://gitee.com/xiaoxuxu233/mirror/raw/master/wizard.lua"))
+getgenv().UILibCache = UILib
+
+local UI = UILib()
+local window = UI:NewWindow("Unnamed")
+local main = window:NewSection("主要功能")
+
+main:CreateToggle("自动做饭", function(enabled)
+    getgenv().cooking = enabled
+    if not enabled then return end
+    
+    while getgenv().cooking do
+        for _ = 1, 6 do
+            fireclickdetector(ThirteenSpicesCD)
+        end
+        task.wait()
+        fireproximityprompt(Kitchen:WaitForChild("Pot").ProximityPrompt)
+    end
+end)
+
+main:CreateToggle("自动吃", function(enabled)
+    getgenv().eating = enabled
+    if not enabled then return end
+    
+    while getgenv().eating do
+        for _, tool in LocalPlayer.Backpack:GetChildren() do
+            tool.Parent = LocalPlayer.Character
+        end
+        for _, tool in LocalPlayer.Character:GetChildren() do
+            if tool:IsA("Tool") then
+                tool:Activate()
+            end
+        end
+        task.wait()
+    end
+end)
