8a5e1cc9e48fa2dba1650b18228f151d1e9b304c
xiaoxuxu2333 <146357705+xiaoxuxu2333@users.noreply.github.com>
2025-08-08 19:16:17 +0800

    更新 natural-disaster-survival-player-killer.lua

--- natural-disaster-survival-player-killer.lua
+++ natural-disaster-survival-player-killer.lua
@@ -7,12 +7,17 @@ local update = RunService.Heartbeat:Connect(function()
 end)
 
 local function getPlayers()
-    local players = game.Players:GetPlayers()
-    table.remove(players, table.find(players, LocalPlayer))
+    local players = {}
 
-    for _, player in players do
-        if player.Character == nil or player.Character:FindFirstChild("Humanoid") == nil or player.Character.Humanoid.Health <= 0 or player.Character:FindFirstChild("HumanoidRootPart") == nil or player.Character.HumanoidRootPart.Velocity.Magnitude > 100 or player.Character.HumanoidRootPart.Position.Magnitude > 2000 then
-            table.remove(players, table.find(players, player))
+    for _, player in game.Players:GetPlayers() do
+        local me = player == LocalPlayer
+        local char = player.Character
+        local hum = char and char:FindFirstChild("Humanoid")
+        local root = hum and hum.RootPart
+        local dying = hum and hum.Health <= 0 and root and root.Parent and (root.Position.Magnitude > 1000 or root.Velocity.Magnitude > 100 or root.Position.Y < 0)
+
+        if not me and char and hum and root and not dying then
+            table.insert(players, player)
         end
     end
 
@@ -21,42 +26,30 @@ end
 
 local random = Random.new()
 
-local parts = {}
+local parts = Instance.new("Folder")
+parts.Name = "Parts"
+parts.Parent = workspace
+
+local queue = {}
 
 local function onPartAdded(part)
     if part:IsA("BasePart") then
-        task.wait(0.5)
-        repeat
-            task.wait()
-        until (part.AssemblyRootPart and part.AssemblyRootPart.Anchored == false) or not part.Parent
+        if part:FindFirstAncestor(workspace.ContentModel.Information.Value) then
+            task.wait(5)
 
-        local ap = Instance.new("AlignPosition")
-        local att = Instance.new("Attachment", part)
-        local bav = Instance.new("BodyAngularVelocity")
-        local bf = Instance.new("BodyForce")
+            local event = Instance.new("BindableEvent")
 
-        ap.Attachment0 = att
-        ap.Responsiveness = 20
-        ap.MaxForce = 10000000
-        ap.MaxVelocity = 500
-        ap.Parent = part
+            queue[part] = event
 
-        bav.MaxTorque = Vector3.one * 1000000000
-        bav.AngularVelocity = Vector3.yAxis * 1000
-        bav.Parent = part
+            event.Event:Wait()
+            event:Destroy()
+        else
+            wait()
+        end
 
-        bf.Force = Vector3.new(0, part:GetMass() * workspace.Gravity, 0)
-        bf.Parent = part
+        if not part.Parent or part.Anchored then return end
 
-        part.CanCollide = false
-
-        table.insert(parts, part)
-    end
-end
-
-local function onPartRemoved(part)
-    if part:IsA("BasePart") then
-        table.remove(parts, table.find(parts, part))
+        part.Parent = parts
     end
 end
 
@@ -67,21 +60,35 @@ for _, part in workspace.Structure:GetDescendants() do
 end
 
 workspace.Structure.DescendantAdded:Connect(onPartAdded)
-workspace.Structure.DescendantRemoving:Connect(onPartRemoved)
 
-while task.wait() do
+while RunService.Heartbeat:Wait() do
+    for part, event in queue do
+        if (part.AssemblyRootPart and part.AssemblyRootPart.Anchored == false) or not part.Parent then
+            event:Fire()
+            queue[part] = nil
+        end
+    end
+
+    for _, v in workspace.Island:GetDescendants() do
+        if v:IsA("BasePart") then
+            v.Velocity = Vector3.zero
+        end
+    end
+
     local players = getPlayers()
 
-    local count = 0
-    for _, v in parts do
-        count += 1
-        local ap = v:FindFirstChild("AlignPosition")
-        if not ap then continue end
-        local char = players[(count % #players) + 1].Character
-        ap.Attachment1 = char and char:FindFirstChild("RootAttachment", true)
-        v.Velocity += random:NextUnitVector() * 100
-        local t = tick() + count * 10
-        ap.Attachment0.Position = Vector3.new(math.cos(t) * v.Size.X / 2, 0, math.sin(t) * v.Size.Z / 2)
+    for i, part in parts:GetChildren() do
+        local char = players[(i % #players) + 1].Character
+        local t = tick() + i * 10
+
+        if char and char:FindFirstChild("HumanoidRootPart") then
+            part.Velocity = ((char.HumanoidRootPart.Position - part.Position).Unit * 1000) + Vector3.new(math.cos(t) * part.Size.X / 2, 0, math.sin(t) * part.Size.Z / 2)
+        else
+            part.Velocity = Vector3.zero
+        end
+
+        part.CanCollide = false
+        part.CanTouch = false
     end
 
     for _, v in workspace.Island:GetDescendants() do
